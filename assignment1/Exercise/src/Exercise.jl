ad7146533d985f9f9485fcba4466ac260c15b8c3
Joshua Niemelä <josh@jniemela.dk>
2024-11-27 19:48:46 +0100

    Did part a to c

--- assignment1/Exercise/src/Exercise.jl
+++ assignment1/Exercise/src/Exercise.jl
@@ -1,5 +1,26 @@
 using Distributions
 using LinearAlgebra
+using GLMakie
+
+
+# These functions come from the proofs in the lecture notes about GLM (week 1)
+# This assumes a prior of mean 0 and variance 1.
+function parameter_estimation(dataset)
+    x, y = dataset
+    k, l = size(x)
+
+    μ = x * inv(var(y) * I(l) + x' * x) * y
+    Σ = I(k) - x * inv(var(y) * I(l) + x' * x) * x'
+
+    return μ, Σ
+end
+
+function posterior_predictive(x, dataset)
+    _, y = dataset
+    μ, Σ = parameter_estimation(dataset)
+
+    Normal(x' * μ, var(y) + x' * Σ * x)
+end
 
 # A begin
 px = MultivariateNormal([0, 0], [1 0; 0 1])
@@ -13,22 +34,27 @@ y = Normal.(x' * θ, 0.1) .|> rand
 dataset = (x, y)
 # A end
 
-#=
-p(y | x, θ) is given
-p(θ) is given
-
-P(θ | x, y) = p(x, y | θ) * p(θ) / p(x, y)
-=#
 # B begin
 prior_θ = MultivariateNormal([0, 0], [1 0; 0 1])
 
-# This assumes a prior of mean 0 and variance 1.
-function parameter_estimation(dataset)
-    x, y = dataset
-    k, l = size(x)
 
-    μ = x * inv(var(y) * I(l) + x' * x) * y
-    Σ = I(k) - x * inv(var(y) * I(l) + x' * x) * x'
+μ, Σ = parameter_estimation(dataset)
 
-    return μ, Σ
-end
+pθ = MultivariateNormal(μ, Symmetric(Σ))
+
+f(x, y) = pdf(pθ, [x; y])
+xs = LinRange(-3, 3, 200)
+ys = LinRange(-3, 3, 200)
+zs = [f(x, y) for x in xs, y in ys]
+
+surface(xs, ys, zs)
+
+# B end
+
+# C begin
+x1s = LinRange(-3, 3, 200)
+x2s = LinRange(-3, 3, 200)
+zs = [var(posterior_predictive([x1, x2], dataset)) for x1 in x1s, x2 in x2s]
+
+surface(x1s, x2s, zs)
+# C end
