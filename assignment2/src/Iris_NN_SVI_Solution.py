6abfaa1769d4d32ae929690bf82a030b15d12b01
Cornelius Sevald-Krause <cornelius.sevald@protonmail.com>
2024-12-15 17:28:20 +0100

    Partly week 4 pyro advanced modelling

---
+++ assignment2/src/Iris_NN_SVI_Solution.py
@@ -0,0 +1,219 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Iris data set: inference with NN / SVI solution
+
+# Import the required Python packages.
+
+import pyro
+import numpy
+import torch
+import sklearn
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+from sklearn import model_selection
+from pyro.infer.mcmc import NUTS, MCMC
+import pyro.distributions as pdist
+import torch.distributions as tdist
+import torch.nn as tnn
+import pyro.nn as pnn
+import arviz as az
+import seaborn as sns
+
+
+# Set some parameters for inference and make reproducible.
+# 
+# 
+
+seed_value = 42  # Replace with your desired seed value
+torch.manual_seed(seed_value)
+pyro.set_rng_seed(seed_value)
+numpy.random.seed(seed_value)
+
+# MAP or diagonal normal?
+MAP=True
+if MAP:
+  MAXIT=2000 # SVI iterations
+  REPORT=200 # Plot ELBO each time after this amount of SVI iterations
+else:
+  MAXIT=100000
+  REPORT=1000
+
+# Number of samples used in prediction
+S=500
+
+
+# Function to evaluate the accuracy of our trained model.
+
+def accuracy(pred, data):
+  """
+  Calculate accuracy of predicted labels (integers).
+
+  pred: predictions, tensor[sample_index, chain_index, data_index, logits]
+  data: actual data (digit), tensor[data_index]
+
+  Prediction is taken as most common predicted value.
+  Returns accuracy (#correct/#total).
+  """
+  n=data.shape[0]
+  correct=0
+  total=0
+  for i in range(0, n):
+      # Get most common prediction value from logits
+      pred_i=int(torch.argmax(torch.sum(pred[:,0,i,:],0)))
+      # Compare prediction with data
+      if int(data[i])==int(pred_i):
+          correct+=1.0
+      total+=1.0
+  # Return fractional accuracy
+  return correct/total
+
+
+# Load the [iris flower data set](https://en.wikipedia.org/wiki/Iris_flower_data_set) set from [scikit-learn](https://sklearn.org/).
+
+# Iris data set
+Dx=4 # Input vector dim
+Dy=3 # Number of labels
+
+iris=sklearn.datasets.load_iris()
+x_all=torch.tensor(iris.data, dtype=torch.float) # Input vector (4D)
+y_all=torch.tensor(iris.target, dtype=torch.int) # Label(3 classes)
+
+# Make training and test set
+x, x_test, y, y_test = sklearn.model_selection.train_test_split(
+    x_all, y_all, test_size=0.33, random_state=42)
+
+print("Data set / test set sizes: %i, %i." % (x.shape[0], x_test.shape[0]))
+
+
+# The probabilistic model, implemented as a callable class. We could also simply use a function.
+# 
+
+class Model:
+    def __init__(self, x_dim=4, y_dim=3, h_dim=5):
+        self.x_dim=x_dim
+        self.y_dim=y_dim
+        self.h_dim=h_dim
+
+    def __call__(self, x, y=None):
+        """
+        We need None for predictive
+        """
+        x_dim=self.x_dim
+        y_dim=self.y_dim
+        h_dim=self.h_dim
+        # Number of observations
+        n=x.shape[0]
+        # standard deviation of Normals
+        sd=1 # EXERCISE: 100->1
+        # Layer 1
+        w1=pyro.sample("w1", pdist.Normal(0, sd).expand([x_dim, h_dim]).to_event(2))
+        b1=pyro.sample("b1", pdist.Normal(0, sd).expand([h_dim]).to_event(1))
+        # Layer 2 # EXERCISE: added layer
+        w2=pyro.sample("w2", pdist.Normal(0, sd).expand([h_dim, h_dim]).to_event(2))
+        b2=pyro.sample("b2", pdist.Normal(0, sd).expand([h_dim]).to_event(1))
+        # Layer 3
+        w3=pyro.sample("w3", pdist.Normal(0, sd).expand([h_dim, y_dim]).to_event(2))
+        b3=pyro.sample("b3", pdist.Normal(0, sd).expand([y_dim]).to_event(1))
+        # NN
+        h1=torch.tanh((x @ w1) + b1)
+        h2=torch.tanh((h1 @ w2) + b2) # EXERCISE: added layer
+        logits=(h2 @ w3 + b3)
+        # Save deterministc variable (logits) in trace
+        pyro.deterministic("logits", logits)
+        # Categorical likelihood
+        with pyro.plate("labels", n):
+            obs=pyro.sample("obs", pdist.Categorical(logits=logits), obs=y)
+
+
+# Make the Model and the Guide objects, and optimize the ELBO.  
+
+# Instantiate the Model object
+model=Model()
+
+if MAP:
+    # MAP estimate of model parameter (here SVI boild down to simple gradient descent)
+    guide=pyro.infer.autoguide.AutoDelta(model)
+else:
+    # Variational estimate of model parameters using diagonal normal (SVI)
+    guide=pyro.infer.autoguide.AutoDiagonalNormal(model)
+
+# Optimizer
+adam=pyro.optim.Adam({"lr": 0.01})
+# SVI
+svi=pyro.infer.SVI(model, guide, adam, loss=pyro.infer.Trace_ELBO())
+
+# Clear any previously used parameters
+pyro.clear_param_store()
+
+# Optimize the ELBO
+elbo=[]
+for j in range(0, MAXIT):
+    loss=svi.step(x, y)
+    elbo.append(loss)
+    if j%REPORT==0:
+        print("[Iteration %04d] loss: %.4f" % (j, loss))
+
+
+# ELBO vs. iteration plot
+plt.xlabel("Iteration")
+plt.ylabel("- ELBO")
+plt.plot(elbo)
+
+
+# Print the estimated parameters.
+
+for name, value in pyro.get_param_store().items():
+    print(name, pyro.param(name))
+
+
+# Get the [posterior predictive distribution](https://en.wikipedia.org/wiki/Posterior_predictive_distribution) by sampling the model's parameters from the Guide object and applying the model to the test set.
+
+# 
+
+guide.requires_grad_(False)
+
+posterior_predictive=pyro.infer.Predictive(model, guide=guide, num_samples=S, return_sites=["logits"])(
+x_test, None
+)
+
+
+# Evaluate the accuracy of the model on the test set.
+# 
+
+# Print accuracy
+logits=posterior_predictive['logits']
+print("Shape of posterior preditive for y (logits):", logits.shape)
+print("Success: %.2f" % accuracy(logits, y_test))
+
+
+# # NUTS inferece
+
+# Perform inference using NUTS
+# Instantiate the Model object
+model=Model()
+
+# NUTS
+nuts_kernel = NUTS(model)
+mcmc = MCMC(nuts_kernel, num_samples=100, warmup_steps=50)
+mcmc.run(x, y)
+
+posterior_samples = mcmc.get_samples()
+
+
+# Diagnostics from Arviz
+nuts_inference = az.from_pyro(mcmc)
+nuts_summary = az.summary(nuts_inference, var_names=["w1", "b1", "w2", "b2", "w3", "b3"])
+
+# Print the summary
+print("NUTS Summary (R-hat and ESS):")
+print(nuts_summary)
+
+
+# Get posterior predictives (logits) from NUTS
+train_predict = pyro.infer.Predictive(model, posterior_samples)(x_test, None)
+# Print accuracy
+logits=train_predict['logits']
+print("Shape of posterior preditive for y (logits):", logits.shape)
+print("Success: %.2f" % accuracy(logits, y_test))
+
