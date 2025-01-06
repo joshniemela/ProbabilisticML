b806c597888a3cdf28bc09778caa341f2e6ae9f7
Joshua Niemelä <josh@jniemela.dk>
2025-01-06 13:50:52 +0100

    Initial DDPM commit

---
+++ exam/src/ddpm.py
@@ -0,0 +1,162 @@
+import torch
+import torch.nn as nn
+
+
+class DDPM(nn.Module):
+    def __init__(self, network, T=100, beta_1=1e-4, beta_T=2e-2):
+        """
+        Initialize Denoising Diffusion Probabilistic Model
+
+        Parameters
+        ----------
+        network: nn.Module
+            The inner neural network used by the diffusion process. Typically a Unet.
+        beta_1: float
+            beta_t value at t=1
+        beta_T: [float]
+            beta_t value at t=T (last step)
+        T: int
+            The number of diffusion steps.
+        """
+
+        super(DDPM, self).__init__()
+
+        # Normalize time input before evaluating neural network
+        # Reshape input into image format and normalize time value before sending it to network model
+        self._network = network
+        self.network = lambda x, t: (
+            self._network(x.reshape(-1, 1, 28, 28), (t.squeeze() / T))
+        ).reshape(-1, 28 * 28)
+
+        # Total number of time steps
+        self.T = T
+
+        # Registering as buffers to ensure they get transferred to the GPU automatically
+        self.register_buffer("beta", torch.linspace(beta_1, beta_T, T + 1))
+        self.register_buffer("alpha", 1 - self.beta)
+        self.register_buffer("alpha_bar", self.alpha.cumprod(dim=0))
+
+    def forward_diffusion(self, x0, t, epsilon):
+        """
+        q(x_t | x_0)
+        Forward diffusion from an input datapoint x0 to an xt at timestep t, provided a N(0,1) noise sample epsilon.
+        Note that we can do this operation in a single step
+
+        Parameters
+        ----------
+        x0: torch.tensor
+            x value at t=0 (an input image)
+        t: int
+            step index
+        epsilon:
+            noise sample
+
+        Returns
+        -------
+        torch.tensor
+            image at timestep t
+        """
+
+        mean = torch.sqrt(self.alpha_bar[t]) * x0
+        std = torch.sqrt(1 - self.alpha_bar[t])
+
+        return mean + std * epsilon
+
+    def reverse_diffusion(self, xt, t, epsilon):
+        """
+        p(x_{t-1} | x_t)
+        Single step in the reverse direction, from x_t (at timestep t) to x_{t-1}, provided a N(0,1) noise sample epsilon.
+
+        Parameters
+        ----------
+        xt: torch.tensor
+            x value at step t
+        t: int
+            step index
+        epsilon:
+            noise sample
+
+        Returns
+        -------
+        torch.tensor
+            image at timestep t-1
+        """
+
+        mean = (
+            1.0
+            / torch.sqrt(self.alpha[t])
+            * (
+                xt
+                - (self.beta[t])
+                / torch.sqrt(1 - self.alpha_bar[t])
+                * self.network(xt, t)
+            )
+        )
+        std = torch.where(
+            t > 0,
+            torch.sqrt(
+                ((1 - self.alpha_bar[t - 1]) / (1 - self.alpha_bar[t])) * self.beta[t]
+            ),
+            0,
+        )
+
+        return mean + std * epsilon
+
+    @torch.no_grad()
+    def sample(self, shape):
+        """
+        Sample from diffusion model (Algorithm 2 in Ho et al, 2020)
+
+        Parameters
+        ----------
+        shape: tuple
+            Specify shape of sampled output. For MNIST: (nsamples, 28*28)
+
+        Returns
+        -------
+        torch.tensor
+            sampled image
+        """
+
+        # Sample xT: Gaussian noise
+        xT = torch.randn(shape).to(self.beta.device)
+
+        xt = xT
+        for t in range(self.T, 0, -1):
+            noise = torch.randn_like(xT) if t > 1 else 0
+            t = torch.tensor(t).expand(xt.shape[0], 1).to(self.beta.device)
+            xt = self.reverse_diffusion(xt, t, noise)
+
+        return xt
+
+    def elbo_simple(self, x0):
+        """
+        ELBO training objective (Algorithm 1 in Ho et al, 2020)
+
+        Parameters
+        ----------
+        x0: torch.tensor
+            Input image
+
+        Returns
+        -------
+        float
+            ELBO value
+        """
+
+        # Sample time step t
+        t = torch.randint(1, self.T, (x0.shape[0], 1)).to(x0.device)
+
+        # Sample noise
+        epsilon = torch.randn_like(x0)
+
+        # TODO: Forward diffusion to produce image at step t
+        xt = self.forward_diffusion(x0, t, epsilon)
+
+        return -nn.MSELoss(reduction="mean")(epsilon, self.network(xt, t))
+
+    def loss(self, x0):
+        """
+        Loss function. Just the negative of the ELBO.
+        """
+        return -self.elbo_simple(x0).mean()
---
+++ exam/src/utils.py
@@ -0,0 +1,47 @@
+# ExponentialMovingAverage implementation as used in pytorch vision
+# https://github.com/pytorch/vision/blob/main/references/classification/utils.py#L159
+
+# BSD 3-Clause License
+
+# Copyright (c) Soumith Chintala 2016,
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+import torch
+
+
+class ExponentialMovingAverage(torch.optim.swa_utils.AveragedModel):
+    """Maintains moving averages of model parameters using an exponential decay.
+    ``ema_avg = decay * avg_model_param + (1 - decay) * model_param``
+    `torch.optim.swa_utils.AveragedModel <https://pytorch.org/docs/stable/optim.html#custom-averaging-strategies>`_
+    is used to compute the EMA.
+    """
+
+    def __init__(self, model, decay, device="cpu"):
+        def ema_avg(avg_model_param, model_param, num_averaged):
+            return decay * avg_model_param + (1 - decay) * model_param
+
+        super().__init__(model, device, ema_avg, use_buffers=True)
