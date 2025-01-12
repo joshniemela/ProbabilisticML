8527d2857c2cc7df3fe08f26eda83427eeec7a84
Haltoen <hjaltipp@hotmail.com>
2025-01-12 14:27:53 +0100

    Refactored A1, (moved functions out of main)

--- exam/src/A1/train_utils.py
+++ exam/src/A1/train_utils.py
@@ -1,11 +1,9 @@
-import torch
-from torchvision import datasets, transforms, utils
-from tqdm.auto import tqdm
-import matplotlib.pyplot as plt
 
+import torch
+from torchvision import transforms, utils
+import matplotlib.pyplot as plt
+from tqdm.auto import tqdm
 from utils import ExponentialMovingAverage
-from ddpm import DDPM, ImportanceDDPM
-from scorenet import ScoreNet
 
 
 def train(
@@ -84,55 +82,6 @@ def train(
         if per_epoch_callback:
             per_epoch_callback(ema_model.module if ema else model)
 
-
-# Parameters
-T = 1000
-learning_rate = 1e-3
-epochs = 100
-batch_size = 256
-
-
-# Rather than treating MNIST images as discrete objects, as done in Ho et al 2020,
-# we here treat them as continuous input data, by dequantizing the pixel values (adding noise to the input data)
-# Also note that we map the 0..255 pixel values to [-1, 1], and that we process the 28x28 pixel values as a flattened 784 tensor.
-transform = transforms.Compose(
-    [
-        transforms.ToTensor(),
-        transforms.Lambda(
-            lambda x: x + torch.rand(x.shape) / 255
-        ),  # Dequantize pixel values
-        transforms.Lambda(lambda x: (x - 0.5) * 2.0),  # Map from [0,1] -> [-1, -1]
-        transforms.Lambda(lambda x: x.flatten()),
-    ]
-)
-
-# Download and transform train dataset
-dataloader_train = torch.utils.data.DataLoader(
-    datasets.MNIST("./mnist_data", download=True, train=True, transform=transform),
-    batch_size=batch_size,
-    shuffle=True,
-)
-
-# Select device
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-# Construct Unet
-# The original ScoreNet expects a function with std for all the
-# different noise levels, such that the output can be rescaled.
-# Since we are predicting the noise (rather than the score), we
-# ignore this rescaling and just set std=1 for all t.
-mnist_unet = ScoreNet((lambda t: torch.ones(1).to(device)))
-
-# Construct model
-model = ImportanceDDPM(mnist_unet, T=T).to(device)
-
-# Construct optimizer
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
-# Setup simple scheduler
-scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, 0.9999)
-
-
 def reporter(model):
     """Callback function used for plotting images during training"""
 
@@ -153,18 +102,3 @@ def reporter(model):
         plt.imshow(transforms.functional.to_pil_image(grid), cmap="gray")
         plt.show()
 
-def report_num_ts(model):
-    print(model.t_num_samples)
-    #print(model.prev_losses)
-
-# Call training loop
-train(
-    model,
-    optimizer,
-    scheduler,
-    dataloader_train,
-    epochs=epochs,
-    device=device,
-    ema=False,
-    per_epoch_callback=reporter
-)
